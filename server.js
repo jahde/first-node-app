6bdc0cea8392856f2a33a486da720196a9cd6fb2
jahde <jahde@classpass.com>
2015-11-14 21:49:53 -0500

    handle file upload

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route, handle) {
 	function onRequest(request, response) {
-		console.log('Request received.');
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write('Hello World');
-		response.end();
+		var postData = '';
+		var pathname = url.parse(request.url).pathname;
+		console.log('Request for ' + pathname + ' received.');
+		route(handle, pathname, response, request);
+
+		// request.setEncoding('utf8');
+		// request.addListener('data', function(postDataChunk) {
+		// 	postData += postDataChunk;
+		// 	console.log('Received POST data chunk "'+
+		// 				postDataChunk + '".');
+		// });
+
+		// request.addListener('end', function(){
+		// 	route(handle, pathname, response, postData);
+		// });
+
+		// response.writeHead(200, {'Content-Type': 'text/plain'});
+
+		// var content = route(handle, pathname);
+		// response.write(content);
+		// response.end();
 	}
 
 	http.createServer(onRequest).listen(8888);
